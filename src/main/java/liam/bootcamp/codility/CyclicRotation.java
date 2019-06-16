b377df3be291d9ccb94aa51330a225e7cdf91e68
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-06-16 22:01:12 +0100

    Solved rotation

--- src/main/java/liam/bootcamp/codility/CyclicRotation.java
+++ src/main/java/liam/bootcamp/codility/CyclicRotation.java
@@ -1,8 +1,29 @@
 package liam.bootcamp.codility;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CyclicRotation {
 
   public static int[] rotate(int[] array, int k) {
-    return new int[] {};
+    for(int i = 0; i < k; i++) {
+      array = rotate(array);
+    }
+    return array;
+  }
+
+  private static int[] rotate(int[] array) {
+    List<Integer> numbers = toList(array);
+    int element = numbers.remove(numbers.size() - 1);
+    numbers.add(0, element);
+    return numbers.stream().mapToInt(Integer::intValue).toArray();
+  }
+
+  private static List<Integer> toList(int[] array) {
+    List<Integer> numbers = new ArrayList<>();
+    for(int i : array) {
+      numbers.add(i);
+    }
+    return numbers;
   }
 }
