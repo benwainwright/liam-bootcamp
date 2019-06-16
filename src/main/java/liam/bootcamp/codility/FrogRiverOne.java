cfb0c94c35e3f715b98a34eec8e4e2dc0e6072a1
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-06-16 23:17:12 +0100

    Solved frog problem

--- src/main/java/liam/bootcamp/codility/FrogRiverOne.java
+++ src/main/java/liam/bootcamp/codility/FrogRiverOne.java
@@ -1,7 +1,20 @@
 package liam.bootcamp.codility;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FrogRiverOne {
   public static int solution(int x, int[] array) {
-    return 0;
+    Set<Integer> leaves = new HashSet<Integer>();
+
+    for(int i = 0; i < array.length; i++) {
+      leaves.add(array[i]);
+
+      if(leaves.size() == x) {
+        return i;
+      }
+    }
+
+    return -1;
   }
 }
