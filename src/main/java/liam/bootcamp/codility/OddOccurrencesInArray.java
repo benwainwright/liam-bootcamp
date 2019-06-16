a45fb0b51f4dd87b68fa588e365d49a49a3add60
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-06-16 20:37:34 +0100

    Added solution for odd occurrences

--- src/main/java/liam/bootcamp/codility/OddOccurrencesInArray.java
+++ src/main/java/liam/bootcamp/codility/OddOccurrencesInArray.java
@@ -1,8 +1,28 @@
 package liam.bootcamp.codility;
 
+import java.util.HashMap;
+
 public class OddOccurrencesInArray {
 
     public static int findOddOccurrences(int[] array) {
-        return 1;
+
+      HashMap<Integer, Integer> numbers = new HashMap<Integer, Integer>();
+      for(int i = 0; i < array.length; i++) {
+        int num = array[i];
+        if(!numbers.containsKey(num)) {
+          numbers.put(num, 1);
+        } else {
+          numbers.put(num, numbers.get(num) + 1);
+        }
+      }
+
+      for(int i : numbers.keySet()) {
+        System.out.println(i);
+        if(numbers.get(i) % 2 != 0) {
+          return i;
+        }
+      }
+
+      return 0;
     }
 }
