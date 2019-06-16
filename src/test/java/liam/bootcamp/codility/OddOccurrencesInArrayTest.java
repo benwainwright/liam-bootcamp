ed75a4dd65af54cc7c098e50ed55d25ceaf64f38
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-06-16 17:02:13 +0100

    All working

--- src/test/java/liam/bootcamp/codility/OddOccurrencesInArrayTest.java
+++ src/test/java/liam/bootcamp/codility/OddOccurrencesInArrayTest.java
@@ -1,11 +1,18 @@
 package liam.bootcamp.codility;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+
 
 class OddOccurrencesInArrayTest {
 
     @Test
     public void testExampleFromSpec() {
         int[] array = {9, 3, 9, 3, 9, 7, 9};
+
+        int actual = OddOccurrencesInArray.findOddOccurrences(array);
+
+        assertEquals(7, actual);
     }
 }
