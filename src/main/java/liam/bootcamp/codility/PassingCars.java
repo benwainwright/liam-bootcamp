102eb9ab7a701bb92fdddd6741f2ee4174f6e61f
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-06-16 23:49:39 +0100

    Added naive passingcars solution

--- src/main/java/liam/bootcamp/codility/PassingCars.java
+++ src/main/java/liam/bootcamp/codility/PassingCars.java
@@ -2,6 +2,19 @@ package liam.bootcamp.codility;
 
 public class PassingCars {
   public static int solution(int[] array) {
-    return 0;
+    int count = 0;
+    for(int i = 0; i < array.length; i++) {
+      if (array[i] == 0) {
+        for(int ii = i + 1; ii < array.length; ii++) {
+          if(array[ii] == 1) {
+            if(count + 1 > 1000000000) {
+              return -1;
+            }
+            count++;
+          }
+        }
+      }
+    }
+    return count;
   }
 }
