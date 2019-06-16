6af1882aaf71188ed6ea9d68eff898068cd6b78a
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-06-16 22:29:45 +0100

    Solved backwards rotation

--- src/main/java/liam/bootcamp/codility/CyclicRotation.java
+++ src/main/java/liam/bootcamp/codility/CyclicRotation.java
@@ -6,10 +6,19 @@ import java.util.List;
 public class CyclicRotation {
 
   public static int[] rotate(int[] array, int k) {
-    for(int i = 0; i < k; i++) {
-      array = rotate(array);
+    for(int i = 0; i < Math.abs(k); i++) {
+      array = k > 0 ? rotate(array) : rotateBackwards(array);
     }
     return array;
+  }
+
+  private static int[] rotateBackwards(int[] array) {
+    List<Integer> numbers = toList(array);
+    if(numbers.size() > 0) {
+      int element = numbers.remove(0);
+      numbers.add(element);
+    }
+    return numbers.stream().mapToInt(Integer::intValue).toArray();
   }
 
   private static int[] rotate(int[] array) {
