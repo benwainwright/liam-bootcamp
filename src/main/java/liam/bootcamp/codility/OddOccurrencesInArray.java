e982703f5fa617ec713167010e467fae0c25207c
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-06-16 16:55:58 +0100

    Initial commit

---
+++ src/main/java/liam/bootcamp/codility/OddOccurrencesInArray.java
@@ -0,0 +1,8 @@
+package liam.bootcamp.codility;
+
+public class OddOccurrencesInArray {
+
+    public static int findOddOccurrences(int[] array) {
+        return 1;
+    }
+}
---
+++ src/test/java/liam/bootcamp/codility/OddOccurrencesInArrayTest.java
@@ -0,0 +1,11 @@
+package liam.bootcamp.codility;
+
+import org.junit.jupiter.api.Test;
+
+class OddOccurrencesInArrayTest {
+
+    @Test
+    public void testExampleFromSpec() {
+        int[] array = {9, 3, 9, 3, 9, 7, 9};
+    }
+}
