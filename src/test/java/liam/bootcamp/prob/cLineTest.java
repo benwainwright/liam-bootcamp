3afe2ec9c8132347e825c11655a472491fc16e49
Liam Hale <leelam.hale@gmail.com>
2019-07-06 19:08:33 +0100

    boop

--- src/test/java/liam/bootcamp/prob/cLineTest.java
+++ src/test/java/liam/bootcamp/prob/cLineTest.java
@@ -8,6 +8,28 @@ public class cLineTest{
     public void removingOneNamedArg(){
         String[] inputArray = {"foo", "--baz", "bar"};
         String[] expected = {"foo"};
-        String[] actual = cLine.removeNamedArgs();
+        String[] actual = cLine.removeNamedArgs(inputArray);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void removingTwoNamedArg(){
+        String[] inputArray = {"foo", "--baz", "bar", "bosh", "--biz", "bop", "bash"};
+        String[] expected = {"foo", "bosh", "bash"};
+        String[] actual = cLine.removeNamedArgs(inputArray);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void removingNoNamedArg(){
+        String[] inputArray = {"foo"};
+        String[] expected = {"foo"};
+        String[] actual = cLine.removeNamedArgs(inputArray);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void removingOneNamedArg(){
+        String[] inputArray = {"foo", "--baz", "bar"};
+        String[] expected = {"foo"};
+        String[] actual = cLine.removeNamedArgs(inputArray);
+        assertEquals(expected, actual);
     }
 }
